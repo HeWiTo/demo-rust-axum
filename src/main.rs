493b828d57ad3fd378f9cb4e940c311b3b250595
Henri <h.w.tompodung@gmail.com>
2023-10-13 14:57:23 +0700

    Respond to multiple HTTP verbs

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,13 @@ pub async fn main() {
         )
         .route("/demo.png", 
             get(get_demo_png)
+        )
+        .route("/foo", 
+            get(get_foo)
+            .put(put_foo)
+            .patch(patch_foo)
+            .post(post_foo)
+            .delete(delete_foo)
         );
 
     // Run our application as a hyper server on http://localhost:3000.
@@ -103,4 +110,34 @@ async fn get_demo_png() -> impl IntoResponse {
         axum::response::Headers([(header::CONTENT_TYPE, "image/png")]),
         base64::decode(png).unwrap(),
     )
+}
+
+// axum handler for "GET /foo" which returns a string message.
+// This shows our naming convention for HTTP GET handlers.
+pub async fn get_foo() -> String {
+   "GET foo".to_string()
+}
+
+// axum handler for "PUT /foo" which returns a string message.
+// This shows our naming convention for HTTP PUT handlers.
+pub async fn put_foo() -> String {
+   "PUT foo".to_string()
+}
+
+// axum handler for "PATCH /foo" which returns a string message.
+// This shows our naming convention for HTTP PATCH handlers.
+pub async fn patch_foo() -> String {
+   "PATCH foo".to_string()
+}
+
+// axum handler for "POST /foo" which returns a string message.
+// This shows our naming convention for HTTP POST handlers.
+pub async fn post_foo() -> String {
+   "POST foo".to_string()
+}
+
+// axum handler for "DELETE /foo" which returns a string message.
+// This shows our naming convention for HTTP DELETE handlers.
+pub async fn delete_foo() -> String {
+   "DELETE foo".to_string()
 }
